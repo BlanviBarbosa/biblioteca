86a4f9c91d0e0b5bb0750d9f556591746e1b068a
Blanvi Barbosa <blanvisb@gmail.com>
2018-02-12 10:25:58 -0200

    Criação da classe pessoa

---
+++ Pessoa.php
@@ -0,0 +1,25 @@
+<?
+/**
+* 
+*/
+class Pessoa
+{
+	public $nome;
+	public $idade;
+	
+}
+
+
+$pessoa1 = new Pessoa();
+$pessoa1->nome 	= "Marcia";
+$pessoa1->idade = 30;
+
+
+$pessoa2 =	new Pessoa();
+$pessoa2->nome 	= "Maria";
+$pessoa2->idade = 20;
+
+
+echo $pessoa1->nome;
+
+?>
